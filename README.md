4b051676785a1ccbba50fb3e2512b14f84ea6e56
PEM-- <pemarchandet@gmail.com>
2014-10-12 12:43:08 +0200

    0.1.0

---
+++ README.md
@@ -0,0 +1,13 @@
+## Description
+This [Meteor](https://www.meteor.com/) package is a plugin for
+[CSSC](https://atmospherejs.com/pierreeric/cssc).
+
+Please refer to [CSSC](https://atmospherejs.com/pierreeric/cssc) for details.
+
+It avoids adding CSS files for Famo.us declaring all styles
+in CoffeeScript.
+
+## Installation
+```bash
+meteor add pierreeric:cssc-famous
+```
--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
   summary: 'Famo.us plugin for CSSC',
   version: '0.1.0',
+  name: 'pierreeric:cssc-famous',
   git: 'https://github.com/PEM--/cssc-famous.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.3.1');
-  api.use('pierreeric:cssc');
-  api.addFiles('cssc-famous.coffee');
+  api.use(['coffeescript', 'pierreeric:cssc']);
+  api.addFiles('cssc-famous.coffee', 'client');
 });
 
 Package.onTest(function(api) {
