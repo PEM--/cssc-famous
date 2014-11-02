f3ce0bb21556f636d4d5068a847b584183189a8b
PEM-- <pemarchandet@gmail.com>
2014-11-02 19:10:53 +0100

    Meteor-1.0.0

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   summary: 'Famo.us plugin for CSSC',
-  version: '0.1.0',
+  version: '1.0.0',
   name: 'pierreeric:cssc-famous',
   git: 'https://github.com/PEM--/cssc-famous.git'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.3.1');
-  api.use(['coffeescript', 'pierreeric:cssc@0.5.0']);
+  api.use('pierreeric:cssc@1.0.0');
   api.addFiles('cssc-famous.coffee', 'client');
+  api.imply('pierreeric:cssc');
 });
 
 Package.onTest(function(api) {
